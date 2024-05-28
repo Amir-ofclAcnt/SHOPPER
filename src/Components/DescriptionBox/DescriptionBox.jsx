90627fd6ae97311d3cdec94da4c2663b35957e00
Amir <hosseiniamir747@gmail.com>
2024-05-28 08:52:52 +0200

    added more files

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+
+      <div className="descriptionbox-description">
+        <p>
+          Upgrade your wardrobe with sustainable style. Ethically-sourced
+          clothing delivered to your door. Shop now! (Focuses on sustainability
+          and convenience){" "}
+        </p>
+        <p>
+          Unleash your inner chef! Discover gourmet ingredients and kitchenware
+          you won't find anywhere else. Explore our culinary haven! (Highlights
+          unique products and creates a sense of discovery)
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ProductDisplay.css";
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
+import { ShopContext } from "../../Context/ShopContext";
 
 const ProductDisplay = (props) => {
   const { product } = props;
+  const { addToCart } = useContext(ShopContext);
+
   return (
     <div className="productdisplay">
       <div className="productdisplay-left">
@@ -14,14 +17,15 @@ const ProductDisplay = (props) => {
           <img src={product.image} alt="" />
           <img src={product.image} alt="" />
         </div>
+        
         <div className="productdisplay-img">
           <img className="productdisplay-main-img" src={product.image} alt="" />
         </div>
       </div>
 
-
       <div className="productdisplay-right">
         <h1>{product.name}</h1>
+
         <div className="productdisplay-right-stars">
           <img src={star_icon} alt="" />
           <img src={star_icon} alt="" />
@@ -30,6 +34,7 @@ const ProductDisplay = (props) => {
           <img src={star_dull_icon} alt="" />
           <p>(122)</p>
         </div>
+
         <div className="productdisplay-right-prices">
           <div className="productdisplay-right-price-old">
             ${product.old_price}
@@ -38,10 +43,12 @@ const ProductDisplay = (props) => {
             ${product.new_price}
           </div>
         </div>
+
         <div className="productdisplay-right-description">
           clothes are crafted with premium materials and meticulous attention to
           detail, ensuring superior quality and comfort.
         </div>
+
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
           <div className="productdisplay-right-sizes">
@@ -52,10 +59,19 @@ const ProductDisplay = (props) => {
             <div>XXL</div>
           </div>
         </div>
-        <button>ADD TO CART</button>
+
+        <button
+          onClick={() => {
+            addToCart(product.id);
+          }}
+        >
+          ADD TO CART
+        </button>
+
         <p className="productdisplay-right-category">
           <span>Category:</span> Women, T-Shirt, Crop Top
         </p>
+
         <p className="productdisplay-right-category">
           <span>Tags:</span> Modern, Latest
         </p>
