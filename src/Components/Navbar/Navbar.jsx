8a5991295147b2e9174420da30fd413aca1b42ac
Amir <hosseiniamir747@gmail.com>
2024-06-03 18:20:31 +0200

    changed some product images

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt="" />
+        <img className="nav-f-logo" src={logo} alt="" />
         <p>Shopper</p>
       </div>
       <ul className="nav-menu">
