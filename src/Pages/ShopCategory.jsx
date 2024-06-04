03158b2de4a8cbb3cfe712cb9cca4fd758443350
Amir <hosseiniamir747@gmail.com>
2024-06-04 17:41:54 +0200

    changed styles and image

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -35,7 +35,7 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
-      <div className="shopcategory-loadmore">Explore More</div>
+      <div className="shopcategory-loadmore">Explore More...</div>
         
     </div>
   );
