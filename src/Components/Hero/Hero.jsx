3a63f3ee1b6c4d0ba47f528753c8c715a7653439
Amir <hosseiniamir747@gmail.com>
2024-06-08 16:52:20 +0200

    styled nav and buttons etc

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-left">
-        <h2>New Arrivals Only</h2>
+        <h2>Summer Season</h2>
         <div>
           <div className="hero-hand-icon">
             <p>new</p>
